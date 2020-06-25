da5e882765540ba1fb020374e0f6993c6f660565
Pahevna <afpavel@inbox.ru>
2020-06-25 21:46:04 +0400

    add UIScrollView and move view when keyboard appears

--- UITextField/UITextField/ViewController.swift
+++ UITextField/UITextField/ViewController.swift
@@ -9,11 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-    
+        
     let myButton: UIButton = {
         let myButton = UIButton()
-        myButton.translatesAutoresizingMaskIntoConstraints = false
-        myButton.frame = CGRect(x: UIScreen.main.bounds.width/4, y: 200, width: 200, height: 31)
+        myButton.frame = CGRect(x: UIScreen.main.bounds.width/4, y: 600, width: 200, height: 31)
         myButton.setTitle("Добавить", for: .normal)
         myButton.setTitleColor(.black, for: .normal)
         myButton.backgroundColor = .white
@@ -25,27 +24,53 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     let myTextField: UITextField = {
         let myTextField = UITextField()
-        myTextField.frame = CGRect(x: UIScreen.main.bounds.width/9, y: 50, width: 300, height: 31)
+        myTextField.frame = CGRect(x: UIScreen.main.bounds.width/9, y: 450, width: 300, height: 31)
         myTextField.borderStyle = .roundedRect
         myTextField.placeholder = "Введите текст"
         myTextField.clearButtonMode = .always
         return myTextField
     }()
-        
+    
+    let myScrollView: UIScrollView = {
+        let myScrollView = UIScrollView()
+        myScrollView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.size.height)
+        myScrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height + 50)
+        return myScrollView
+    }()
+    
     var textFieldArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(myButton)
-        view.addSubview(myTextField)
+        view.addSubview(myScrollView)
+        myScrollView.addSubview(myButton)
+        myScrollView.addSubview(myTextField)
         myTextField.delegate = self
+        registerForKeybordNotifications()
+    }
+    
+    func registerForKeybordNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func kbWillShow(_ notification: Notification) {
+        let userInfo = notification.userInfo
+        let kbFrameSize = (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        myScrollView.contentOffset = CGPoint(x: 0, y: kbFrameSize.height)
+    }
+    
+    @objc func kbWillHide() {
+        myScrollView.contentOffset = CGPoint.zero
     }
     
     @objc func pressedButton(_ sender: UIButton) {
-        if myTextField.text?.isEmpty == false {
-            textFieldArray.append(myTextField.text!)
+        guard let text = myTextField.text, !text.isEmpty else {
+            return
         }
+        textFieldArray.append(text)
+        myTextField.resignFirstResponder()
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn: NSRange, replacementString: String) -> Bool {
