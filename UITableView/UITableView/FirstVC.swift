63a9df2e564620df10db0fa3a584e0cd963a02ca
Pahevna <afpavel@inbox.ru>
2020-07-15 18:25:03 +0400

    Create UITableViewCell programmatically. Setting 'rootViewController' results in black screen on device only

---
+++ UITableView/UITableView/FirstVC.swift
@@ -0,0 +1,18 @@
+//
+//  FirstVC.swift
+//  UITableView
+//
+//  Created by mac on 15.07.2020.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class FirstVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red 
+    }
+}
---
+++ UITableView/UITableView/SecondVC.swift
@@ -0,0 +1,18 @@
+//
+//  SecondVC.swift
+//  UITableView
+//
+//  Created by mac on 15.07.2020.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
