e304fbbf68827b36ee4c77a650ae179da9073339
Pahevna <afpavel@inbox.ru>
2021-04-24 22:58:35 +0400

    clear branch

---
+++ datebook/datebook/EventListModule/JsonEventsService.swift
@@ -0,0 +1,29 @@
+//
+//  JsonEventsService.swift
+//  dateBook
+//
+//  Created by mac on 24.04.2021.
+//
+
+import Foundation
+
+protocol EventServiceProtocol {
+    func getEvents(completion: @escaping (Result<[EventListModel]?, Error>) -> Void)
+}
+
+class JsonEventsService: EventServiceProtocol {
+    func getEvents(completion: @escaping (Result<[EventListModel]?, Error>) -> Void) {
+        guard let path = Bundle.main.path(forResource: "events", ofType: "json") else { return }
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            let jsonData = try Data(contentsOf: url)
+            let event = try JSONDecoder().decode([EventListModel].self, from: jsonData)
+            completion(.success(event))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+}
+    
+
